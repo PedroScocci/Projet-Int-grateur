b3f339368c72e9fbaf9d4a6bdf52701e96bab52e
Émile <emins@live.ca>
2019-02-01 10:14:11 -0500

    Création Force Centripète
    Activity Force Centripète

---
+++ app/src/main/java/com/example/scoccipe/projetphysique/Force_Centripete.java
@@ -0,0 +1,24 @@
+package com.example.scoccipe.projetphysique;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class Force_Centripete extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_force__centripete);
+
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(Force_Centripete.this,"fuck you cunt", Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+}
