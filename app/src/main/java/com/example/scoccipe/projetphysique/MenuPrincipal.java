fcd65a1c12c39de193e1171727c9fbb28231168e
Adam <a.april@live.ca>
2019-02-01 10:17:59 -0500

    Creation du bouton

--- app/src/main/java/com/example/scoccipe/projetphysique/MenuPrincipal.java
+++ app/src/main/java/com/example/scoccipe/projetphysique/MenuPrincipal.java
@@ -1,7 +1,10 @@
 package com.example.scoccipe.projetphysique;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MenuPrincipal extends AppCompatActivity {
 
@@ -9,5 +12,13 @@ public class MenuPrincipal extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_principal);
+
+        Button but = (Button) findViewById(R.id.Paradoxe);
+        but.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent myIntent = new Intent(MenuPrincipal.this, Paradoxe.class);
+                startActivity(myIntent);
+            }
+        });
     }
 }
