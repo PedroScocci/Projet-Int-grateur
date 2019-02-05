e6d54573ba15f0e8036223c183403d008cbd3dd1
Adam <a.april@live.ca>
2019-02-05 09:44:56 -0500

    Creation des string et de la seconde activite
    Creation de toutes les variables string necessaires pour le test et de la seconde activite ou les renseignements sur le voyage sont demandees

--- app/src/main/java/com/example/scoccipe/projetphysique/Paradoxe.java
+++ app/src/main/java/com/example/scoccipe/projetphysique/Paradoxe.java
@@ -1,7 +1,10 @@
 package com.example.scoccipe.projetphysique;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Paradoxe extends AppCompatActivity {
 
@@ -9,5 +12,13 @@ public class Paradoxe extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_paradoxe);
+
+        Button but = (Button) findViewById(R.id.boutonStart);
+        but.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent myIntent = new Intent(Paradoxe.this, Paradoxe2.class);
+                startActivity(myIntent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/scoccipe/projetphysique/Paradoxe2.java
@@ -0,0 +1,13 @@
+package com.example.scoccipe.projetphysique;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Paradoxe2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_paradoxe2);
+    }
+}
