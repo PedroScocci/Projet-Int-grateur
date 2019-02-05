f7fa68a054c0abb5223fb4c55b68a0acebdeafea
Pedro Scoccimarro <pedroscocci@oulook.com>
2019-02-05 10:16:18 -0500

    Layout et Toolbar
    Modification du layout des boutons dans l'activity main et ajout d'un menu.

--- app/src/main/java/com/example/scoccipe/projetphysique/MenuPrincipal.java
+++ app/src/main/java/com/example/scoccipe/projetphysique/MenuPrincipal.java
@@ -2,6 +2,8 @@ package com.example.scoccipe.projetphysique;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -44,5 +46,23 @@ public class MenuPrincipal extends AppCompatActivity {
                 Toast.makeText(MenuPrincipal.this, "ONDES", Toast.LENGTH_LONG).show();
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.maintoolbar, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_apropos:
+                Toast.makeText(MenuPrincipal.this, "A PROPOS", Toast.LENGTH_LONG).show();
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
