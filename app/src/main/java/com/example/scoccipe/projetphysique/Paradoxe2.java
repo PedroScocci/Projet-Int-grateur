58648173cdfd3c0e01155417f82fee2cf4795e33
Adam <a.april@live.ca>
2019-02-05 11:07:08 -0500

    Creation act 3 et fin act 2
    Finalisation de la partie fonctionnelle de l'activite 2 et creation de l'activite 3

--- app/src/main/java/com/example/scoccipe/projetphysique/Paradoxe2.java
+++ app/src/main/java/com/example/scoccipe/projetphysique/Paradoxe2.java
@@ -1,7 +1,10 @@
 package com.example.scoccipe.projetphysique;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Paradoxe2 extends AppCompatActivity {
 
@@ -9,5 +12,12 @@ public class Paradoxe2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_paradoxe2);
+        Button but = (Button) findViewById(R.id.paradoxe2);
+        but.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent myIntent = new Intent(Paradoxe2.this, Paradoxe3.class);
+                startActivity(myIntent);
+            }
+        });
     }
 }
