65d5ee1fa43339d80f00981d791a5933a81c4f71
Adam <a.april@live.ca>
2019-02-09 15:09:48 -0500

    Animation
    Début de la programmation de l'animation. Plusieurs bugs à corriger dont la disparition de l'image lors de l'annimation

--- app/src/main/java/com/example/scoccipe/projetphysique/Paradoxe3.java
+++ app/src/main/java/com/example/scoccipe/projetphysique/Paradoxe3.java
@@ -2,6 +2,9 @@ package com.example.scoccipe.projetphysique;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.animation.ObjectAnimator;
+
 
 public class Paradoxe3 extends AppCompatActivity {
 
@@ -9,5 +12,12 @@ public class Paradoxe3 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_paradoxe3);
+        ImageView fusee = (ImageView) findViewById(R.id.fus);
+        fusee.setImageResource(R.drawable.fusee);
+
+        ObjectAnimator animation = ObjectAnimator.ofFloat(fusee, "translationY", 1000f);
+        animation.setDuration(5000);
+        animation.start();
+
     }
 }
