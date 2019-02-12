97703fb77c44e855c1e3a597fc9dff727fad8252
Pedro Scoccimarro <pedroscocci@oulook.com>
2019-02-12 11:09:10 -0500

    Intents 2.0
    Début de transfert d'informations entre l'activity paramètres et l'activity de simulation.

--- app/src/main/java/com/example/scoccipe/projetphysique/GraviteActivity.java
+++ app/src/main/java/com/example/scoccipe/projetphysique/GraviteActivity.java
@@ -1,13 +1,50 @@
 package com.example.scoccipe.projetphysique;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class GraviteActivity extends AppCompatActivity {
+    private Intent intenteru = getIntent();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gravite);
+
+
+
+
+
+        Button bouton_planete = (Button) findViewById(R.id.bouton_planete);
+        bouton_planete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(GraviteActivity.this,
+                        intenteru.getStringExtra("planete"),
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        Button bouton_masse = (Button) findViewById(R.id.bouton_masse);
+        bouton_masse.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(GraviteActivity.this,
+                        intenteru.getStringExtra("masse"),
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        Button bouton_retour = (Button) findViewById(R.id.bouton_retour);
+        bouton_retour.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
 }
--- app/src/main/java/com/example/scoccipe/projetphysique/ParametresGravite.java
+++ app/src/main/java/com/example/scoccipe/projetphysique/ParametresGravite.java
@@ -1,17 +1,20 @@
 package com.example.scoccipe.projetphysique;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Toast;
 
 public class ParametresGravite extends AppCompatActivity {
     private Button bouton_ok;
     private Spinner spinner_planete, spinner_objets;
+    private String planete, objet, masse, hauteur;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,7 +31,22 @@ public class ParametresGravite extends AppCompatActivity {
         bouton_ok.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(ParametresGravite.this, "OK", Toast.LENGTH_LONG).show();
+
+                Intent intent = new Intent(ParametresGravite.this, GraviteActivity.class);
+
+                EditText masse_text = (EditText)  findViewById(R.id.masse_obj1);
+                masse = masse_text.getText().toString();
+
+                Toast.makeText(ParametresGravite.this, masse, Toast.LENGTH_SHORT).show();
+
+                EditText hauteur_text = (EditText) findViewById(R.id.hauteur);
+                hauteur = hauteur_text.getText().toString();
+
+
+                intent.putExtra("masse", masse);
+                intent.putExtra("planete", planete);
+
+                startActivity(intent);
             }
         });
     }
@@ -39,6 +57,7 @@ public class ParametresGravite extends AppCompatActivity {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
                 //Toast.makeText(parent.getContext(), parent.getItemAtPosition(pos).toString(), Toast.LENGTH_SHORT).show();
+                planete = parent.getItemAtPosition(pos).toString();
             }
 
             @Override
@@ -53,7 +72,8 @@ public class ParametresGravite extends AppCompatActivity {
         spinner_objets.setOnItemSelectedListener(new OnItemSelectedListener(){
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
-                Toast.makeText(parent.getContext(), parent.getItemAtPosition(pos).toString(), Toast.LENGTH_SHORT).show();
+                //Toast.makeText(parent.getContext(), parent.getItemAtPosition(pos).toString(), Toast.LENGTH_SHORT).show();
+                objet = parent.getItemAtPosition(pos).toString();
             }
 
             @Override
