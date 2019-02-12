3524bd0b461386b520228f9f06afef3566aa307c
Adam <a.april@live.ca>
2019-02-12 09:56:56 -0500

    Animation fonctionnelle
    Animation de base fonctionnelle. Changements a venir

--- app/src/main/java/com/example/scoccipe/projetphysique/Paradoxe3.java
+++ app/src/main/java/com/example/scoccipe/projetphysique/Paradoxe3.java
@@ -4,6 +4,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ImageView;
 import android.animation.ObjectAnimator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.Animator;
+import android.widget.Toast;
 
 
 public class Paradoxe3 extends AppCompatActivity {
@@ -13,11 +16,15 @@ public class Paradoxe3 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_paradoxe3);
         ImageView fusee = (ImageView) findViewById(R.id.fus);
-        fusee.setImageResource(R.drawable.fusee);
+        //fusee.setImageResource(R.drawable.fusee);
 
-        ObjectAnimator animation = ObjectAnimator.ofFloat(fusee, "translationY", 1000f);
+        ObjectAnimator animation = ObjectAnimator.ofFloat(fusee, "translationY", 500f);
         animation.setDuration(5000);
         animation.start();
 
-    }
-}
+        animation.addListener(new AnimatorListenerAdapter() {
+            public void onAnimationEnd(Animator animation) {
+                Toast.makeText(Paradoxe3.this, "DONE", Toast.LENGTH_LONG).show();
+                }
+            });
+        }}
