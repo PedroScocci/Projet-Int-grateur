1d1d400fbb6c2dc24f27632221c194f1baeb23c0
Adam <a.april@live.ca>
2019-02-12 10:37:03 -0500

    Animation diagonale
    Animation diagonale fonctionnelle, mais manque toujours le fade out en plus

--- app/src/main/java/com/example/scoccipe/projetphysique/Paradoxe3.java
+++ app/src/main/java/com/example/scoccipe/projetphysique/Paradoxe3.java
@@ -7,6 +7,8 @@ import android.animation.ObjectAnimator;
 import android.animation.AnimatorListenerAdapter;
 import android.animation.Animator;
 import android.widget.Toast;
+import android.animation.AnimatorSet;
+import android.view.animation.LinearInterpolator;
 
 
 public class Paradoxe3 extends AppCompatActivity {
@@ -15,16 +17,31 @@ public class Paradoxe3 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_paradoxe3);
-        ImageView fusee = (ImageView) findViewById(R.id.fus);
+        final ImageView fusee = (ImageView) findViewById(R.id.fus);
         //fusee.setImageResource(R.drawable.fusee);
 
-        ObjectAnimator animation = ObjectAnimator.ofFloat(fusee, "translationY", 500f);
+        AnimatorSet animSetXY = new AnimatorSet();
+
+        ObjectAnimator y = ObjectAnimator.ofFloat(fusee,"translationY",fusee.getY(), 2000);
+
+        ObjectAnimator x = ObjectAnimator.ofFloat(fusee,"translationX", fusee.getX(), 1000);
+
+        animSetXY.playTogether(x, y);
+        animSetXY.setInterpolator(new LinearInterpolator());
+        animSetXY.setDuration(7000);
+        animSetXY.start();
+
+        /*ObjectAnimator animation = ObjectAnimator.ofFloat(fusee, "translationY", 2000f);
         animation.setDuration(5000);
         animation.start();
 
         animation.addListener(new AnimatorListenerAdapter() {
             public void onAnimationEnd(Animator animation) {
                 Toast.makeText(Paradoxe3.this, "DONE", Toast.LENGTH_LONG).show();
+                ObjectAnimator animation2 = ObjectAnimator.ofFloat(fusee, "translationX", 1200f);
+                animation2.setDuration(5000);
+                animation2.start();
                 }
-            });
+            });*/
+
         }}
