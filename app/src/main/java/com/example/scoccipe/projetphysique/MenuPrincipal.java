0177660482a2ceaca2f120ee317e6dc6e314c3a8
Émile <emins@live.ca>
2019-02-19 11:00:44 -0500

    Debut intents et calcul parametres
    Commencer le déplacement entre activités et debut du calcul avec les parametres

--- app/src/main/java/com/example/scoccipe/projetphysique/MenuPrincipal.java
+++ app/src/main/java/com/example/scoccipe/projetphysique/MenuPrincipal.java
@@ -1,13 +1,24 @@
 package com.example.scoccipe.projetphysique;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MenuPrincipal extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_principal);
+
+        Button button = (Button) findViewById(R.id.ForceCentripete);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MenuPrincipal.this, ParametresCentripete.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/scoccipe/projetphysique/ParametresCentripete.java
+++ app/src/main/java/com/example/scoccipe/projetphysique/ParametresCentripete.java
@@ -1,11 +1,17 @@
 package com.example.scoccipe.projetphysique;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 public class ParametresCentripete extends AppCompatActivity {
+    public static final String PARAMETRES = "param";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,5 +23,47 @@ public class ParametresCentripete extends AppCompatActivity {
                 R.array.routes_array, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
+
+        TextView tv1 = (TextView) findViewById(R.id.edittext1);
+        int masse  = Integer.valueOf(tv1.toString());
+        TextView tv2 = (TextView) findViewById(R.id.edittext2);
+        int vitesse = Integer.valueOf(tv2.toString());
+        TextView tv3 = (TextView) findViewById(R.id.edittext3);
+        int rayon = Integer.valueOf(tv3.toString());
+
+        final double[] def = new double[1];
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener()
+        {
+            public void onItemSelected(AdapterView<?> parent, View view, int pos, long id)
+            {
+                switch(pos)
+                {
+                    case 0: def[0] = 0.2;
+                    break;
+                    case 1: def[0] = 0.899;
+                    break;
+                    case 2: def[0] = 0.876;
+                    break;
+                    case 3: def[0] = 0.745;
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+        double test = def[0]*3;
+
+        Button button = (Button) findViewById(R.id.button5);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ParametresCentripete.this, Force_Centripete.class);
+                intent.putExtra(PARAMETRES, masse);
+                startActivity(intent);
+            }
+        });
     }
 }
