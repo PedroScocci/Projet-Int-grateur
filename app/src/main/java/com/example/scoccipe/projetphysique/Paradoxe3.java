1d982515d97ce208d01e19289b7fce8434afdee6
Adam <a.april@live.ca>
2019-03-22 09:55:00 -0400

    Retour aux animations android
    Debut du setup de l'animation et differents test faits. Bon parcours de l'image. Plusieurs choses encore a ajouter

--- app/src/main/java/com/example/scoccipe/projetphysique/Paradoxe3.java
+++ app/src/main/java/com/example/scoccipe/projetphysique/Paradoxe3.java
@@ -3,7 +3,9 @@ package com.example.scoccipe.projetphysique;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.animation.AccelerateInterpolator;
 import android.view.animation.AnimationSet;
+import android.view.animation.DecelerateInterpolator;
 import android.webkit.WebView;
 import android.widget.ImageView;
 import android.animation.ObjectAnimator;
@@ -19,15 +21,10 @@ import android.view.animation.Animation;
 public class Paradoxe3 extends AppCompatActivity {
 
     private Intent intent;
-    WebView myBrowser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        myBrowser = (WebView)findViewById(R.id.myBrowser);
-        myBrowser.loadUrl(("file:///C:/Users/Utilisateur/Desktop/Cegep/Session4/Programmation/Intgration/Logs/WebGl/index.html"));
-        myBrowser.getSettings().setJavaScriptEnabled(true);
 
 
         setContentView(R.layout.activity_paradoxe3);
@@ -38,25 +35,50 @@ public class Paradoxe3 extends AppCompatActivity {
         Toast.makeText(this, intent.getStringExtra("ageD"), Toast.LENGTH_SHORT).show();
         AnimatorSet animSetXY = new AnimatorSet();
 
-        ObjectAnimator y = ObjectAnimator.ofFloat(fusee,"translationY",fusee.getY(), 2000);
+        ObjectAnimator y = ObjectAnimator.ofFloat(fusee,"translationY",fusee.getY(), -2000);
 
         ObjectAnimator x = ObjectAnimator.ofFloat(fusee,"translationX", fusee.getX(), 1000);
 
         ObjectAnimator fadeOut = ObjectAnimator.ofFloat(fusee, "alpha",  1f, 0f);
         fadeOut.setDuration(2000);
 
-        animSetXY.playTogether(x, y, fadeOut);
-        animSetXY.setInterpolator(new LinearInterpolator());
+        ObjectAnimator scaleX = ObjectAnimator.ofFloat(fusee,"scaleX", 1f, 0f);
+        ObjectAnimator scaleY = ObjectAnimator.ofFloat(fusee,"scaleY", 1f, 0f);
+
+        animSetXY.playTogether(x, y);
+        animSetXY.setInterpolator(new AccelerateInterpolator());
         animSetXY.setDuration(7000);
         animSetXY.start();
 
         animSetXY.addListener(new AnimatorListenerAdapter() {
             public void onAnimationEnd(Animator animation) {
-                Toast.makeText(Paradoxe3.this, "DONE", Toast.LENGTH_LONG).show();
-                Intent myIntent = new Intent(Paradoxe3.this, Paradoxe4.class);
-                myIntent.putExtra("ageN", intent.getStringExtra("ageN"));
-                myIntent.putExtra("ageD", intent.getStringExtra("ageD"));
-                //startActivity(myIntent);
+
+                ObjectAnimator xRetour = ObjectAnimator.ofFloat(fusee, "translationX", fusee.getX(), -75);
+                xRetour.setDuration(1000);
+                xRetour.start();
+
+                xRetour.addListener(new AnimatorListenerAdapter(){
+                    public void onAnimationEnd(Animator animation){
+                        ObjectAnimator yRetour = ObjectAnimator.ofFloat(fusee, "translationY",200);
+                        ObjectAnimator scaleXRetour = ObjectAnimator.ofFloat(fusee, "scaleX", 0f,1f);
+                        ObjectAnimator scaleYRetour = ObjectAnimator.ofFloat(fusee, "scaleY", 0f,1f);
+                        AnimatorSet animRetour = new AnimatorSet();
+                        animRetour.playTogether(yRetour);
+                        animRetour.setInterpolator(new DecelerateInterpolator());
+                        animRetour.setDuration(5000);
+                        animRetour.start();
+
+                        animRetour.addListener(new AnimatorListenerAdapter() {
+                            @Override
+                            public void onAnimationEnd(Animator animation) {
+                                Intent myIntent = new Intent(Paradoxe3.this, Paradoxe4.class);
+                                myIntent.putExtra("ageN", intent.getStringExtra("ageN"));
+                                myIntent.putExtra("ageD", intent.getStringExtra("ageD"));
+                                startActivity(myIntent);
+                            }
+                        });
+                    }
+                });
                 }
             });
 
