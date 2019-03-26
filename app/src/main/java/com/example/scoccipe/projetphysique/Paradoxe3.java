11ebc6269672068ca82c4e9b23e6cb8fce6466ba
Adam <a.april@live.ca>
2019-03-26 11:05:40 -0400

    Path des animations
    Path des animations complété! Il reste a remplcer les android par les bonnes images et le desing!

--- app/src/main/java/com/example/scoccipe/projetphysique/Paradoxe3.java
+++ app/src/main/java/com/example/scoccipe/projetphysique/Paradoxe3.java
@@ -5,12 +5,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.animation.AccelerateInterpolator;
 import android.view.animation.AnimationSet;
+import android.view.animation.BounceInterpolator;
 import android.view.animation.DecelerateInterpolator;
 import android.webkit.WebView;
 import android.widget.ImageView;
 import android.animation.ObjectAnimator;
 import android.animation.AnimatorListenerAdapter;
 import android.animation.Animator;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 import android.animation.AnimatorSet;
 import android.view.animation.LinearInterpolator;
@@ -31,7 +33,24 @@ public class Paradoxe3 extends AppCompatActivity {
         intent = getIntent();
 
         final ImageView fusee = (ImageView) findViewById(R.id.fus);
+        final ImageView perso = (ImageView) findViewById(R.id.pers);
         //fusee.setImageResource(R.drawable.fusee);
+
+        ObjectAnimator Yperso = ObjectAnimator.ofFloat(perso, "translationY", perso.getY(), -2000);
+        Yperso.setDuration(8000);
+        Yperso.setInterpolator(new AccelerateInterpolator());
+        Yperso.start();
+
+        Yperso.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                ObjectAnimator Yrebond = ObjectAnimator.ofFloat(perso, "translationY",200);
+                Yrebond.setDuration(5000);
+                Yrebond.setInterpolator(new BounceInterpolator());
+                Yrebond.start();
+            }
+        });
+
         Toast.makeText(this, intent.getStringExtra("ageD"), Toast.LENGTH_SHORT).show();
         AnimatorSet animSetXY = new AnimatorSet();
 
@@ -45,13 +64,15 @@ public class Paradoxe3 extends AppCompatActivity {
         ObjectAnimator scaleX = ObjectAnimator.ofFloat(fusee,"scaleX", 1f, 0f);
         ObjectAnimator scaleY = ObjectAnimator.ofFloat(fusee,"scaleY", 1f, 0f);
 
-        animSetXY.playTogether(x, y);
+        animSetXY.playTogether(x, y, scaleX, scaleY);
         animSetXY.setInterpolator(new AccelerateInterpolator());
         animSetXY.setDuration(7000);
         animSetXY.start();
 
         animSetXY.addListener(new AnimatorListenerAdapter() {
             public void onAnimationEnd(Animator animation) {
+
+                Toast.makeText(Paradoxe3.this, String.valueOf(perso.getY()), Toast.LENGTH_LONG).show();
 
                 ObjectAnimator xRetour = ObjectAnimator.ofFloat(fusee, "translationX", fusee.getX(), -75);
                 xRetour.setDuration(1000);
@@ -63,7 +84,7 @@ public class Paradoxe3 extends AppCompatActivity {
                         ObjectAnimator scaleXRetour = ObjectAnimator.ofFloat(fusee, "scaleX", 0f,1f);
                         ObjectAnimator scaleYRetour = ObjectAnimator.ofFloat(fusee, "scaleY", 0f,1f);
                         AnimatorSet animRetour = new AnimatorSet();
-                        animRetour.playTogether(yRetour);
+                        animRetour.playTogether(yRetour, scaleXRetour, scaleYRetour);
                         animRetour.setInterpolator(new DecelerateInterpolator());
                         animRetour.setDuration(5000);
                         animRetour.start();
@@ -81,5 +102,4 @@ public class Paradoxe3 extends AppCompatActivity {
                 });
                 }
             });
-
         }}
