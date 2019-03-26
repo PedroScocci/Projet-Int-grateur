6a420d8b4577a522a6df80657a4ea4c948c9bd87
efe <emins@live.ca>
2019-03-26 11:12:41 -0400

    Dessins et début animations
    Dessins voiture et route et animation de la voiture

--- app/src/main/java/com/example/scoccipe/projetphysique/Force_Centripete.java
+++ app/src/main/java/com/example/scoccipe/projetphysique/Force_Centripete.java
@@ -1,8 +1,14 @@
 package com.example.scoccipe.projetphysique;
 
+import android.animation.ObjectAnimator;
 import android.content.Intent;
+import android.graphics.Path;
+import android.graphics.RectF;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 public class Force_Centripete extends AppCompatActivity {
@@ -14,6 +20,8 @@ public class Force_Centripete extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_force__centripete);
 
+        float x = 500, y = 500, r = 500, angle = 50;
+
         intent = getIntent();
         derapage = intent.getBooleanExtra("derap", ParametresCentripete.derap);
         if(derapage) {
@@ -22,5 +30,28 @@ public class Force_Centripete extends AppCompatActivity {
         else {
             Toast.makeText(Force_Centripete.this,"deg" , Toast.LENGTH_LONG).show();
         }
+
+        Button b = findViewById(R.id.buttonsim);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(derapage){
+                    ImageView iv = findViewById(R.id.voit);
+                    Path path = new Path();
+                    iv.animate().rotation(-360);
+                    iv.animate().setDuration(10000);
+
+                    path.arcTo(0f,30f,200f,1050f,-90f,-359,true);
+                    ObjectAnimator animator = ObjectAnimator.ofFloat(iv,View.X,View.Y,path);
+                    animator.setDuration(10000);
+                    animator.start();
+                }
+                else{
+
+                }
+            }
+        });
+
+
     }
 }
