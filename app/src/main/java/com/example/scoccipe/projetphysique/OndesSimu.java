91ec086a9e51a8ceb955a440896c52f454b75629
GoodLooser <ilibou55555@gmail.com>
2019-03-29 10:19:02 -0400

    Chronomètre sur l'animation
    J'ai implémenter un chronomètre pour tenter de changer l'image à partir d'un certain temps.

--- app/src/main/java/com/example/scoccipe/projetphysique/OndesSimu.java
+++ app/src/main/java/com/example/scoccipe/projetphysique/OndesSimu.java
@@ -1,18 +1,32 @@
 package com.example.scoccipe.projetphysique;
 
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
+import android.os.Handler;
+import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Chronometer;
+import android.widget.ImageView;
 import android.widget.Toast;
+
+import java.util.Timer;
 
 public class OndesSimu extends AppCompatActivity {
 
     private Intent intent;
+    private int images[];
     private double mode;
+    private Chronometer time;
+    private boolean started = false;
+    private long temps,tempsPasser;
+    private Button start;
+    ImageView corde;
+    Handler custom = new Handler();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,11 +38,60 @@ public class OndesSimu extends AppCompatActivity {
 
         Toast.makeText(OndesSimu.this, String.valueOf(mode), Toast.LENGTH_LONG).show();
 
-        Button start = (Button) findViewById(R.id.bOndesSimu);
+        time = (Chronometer) findViewById(R.id.timer);
+        corde = (ImageView) findViewById(R.id.cordeAnimation);
+
+        images = new int[]{R.drawable.corde_onde_1, R.drawable.corde_onde_27, R.drawable.corde_onde_r_26};
+
+        start = (Button) findViewById(R.id.bOndesSimu);
         start.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                
+                if(!started){
+                    
+
+                    start.setText("ARRÊTER");
+                }
+                else if(started){
+
+                    start.setText("COMMENCER");
+
+                    Toast.makeText(OndesSimu.this, String.valueOf(time.getBase()), Toast.LENGTH_LONG).show();
+                }
+                /*while (!started){
+                    for(int i = 1; i<=27; i++)
+                    {
+                        String fileName = "drawable/corde_onde_" + i + ".png";
+                        int imageResource = getResources().getIdentifier(fileName, null, getPackageName());
+                        ImageView bob = (ImageView) findViewById(R.id.cordeAnimation);
+                        Drawable image = getResources().getDrawable(imageResource);
+                        bob.setImageDrawable(image);
+                    }
+                    for(int i = 27; i<=1; i--)
+                    {
+                        String fileName = "drawable/corde_onde_" + i + ".png";
+                        int imageResource = getResources().getIdentifier(fileName, null, getPackageName());
+                        ImageView bob = (ImageView) findViewById(R.id.cordeAnimation);
+                        Drawable image = getResources().getDrawable(imageResource);
+                        bob.setImageDrawable(image);
+                    }
+                    for(int i = 1; i<=27; i++)
+                    {
+                        String fileName = "drawable/corde_onde_r_" + i + ".png";
+                        int imageResource = getResources().getIdentifier(fileName, null, getPackageName());
+                        ImageView bob = (ImageView) findViewById(R.id.cordeAnimation);
+                        Drawable image = getResources().getDrawable(imageResource);
+                        bob.setImageDrawable(image);
+                    }
+                    for(int i = 27; i<=1; i--)
+                    {
+                        String fileName = "drawable/corde_onde_r_" + i + ".png";
+                        int imageResource = getResources().getIdentifier(fileName, null, getPackageName());
+                        ImageView bob = (ImageView) findViewById(R.id.cordeAnimation);
+                        Drawable image = getResources().getDrawable(imageResource);
+                        bob.setImageDrawable(image);
+                    }
+                }*/
             }
         });
     }
@@ -56,4 +119,23 @@ public class OndesSimu extends AppCompatActivity {
                     return super.onOptionsItemSelected(item);
         }
     }
+
+    void chronoCommencer(){
+        started = true;
+
+        time.setBase(SystemClock.elapsedRealtime());
+        time.start();
+
+        //tempsPasser = SystemClock.elapsedRealtime();
+
+        corde.setImageResource(images[1]);
+    }
+
+    void chronoArreter(){
+        started = false;
+
+        time.stop();
+    }
+
+
 }
