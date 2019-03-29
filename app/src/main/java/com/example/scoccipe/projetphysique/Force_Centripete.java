ca9317402b22ae3d4568b28a855c3eb7d0b74708
efe <emins@live.ca>
2019-03-29 10:17:56 -0400

    Animation si dérape pas
    Finalisation de l'animation quand l'auto ne dérape pas

--- app/src/main/java/com/example/scoccipe/projetphysique/Force_Centripete.java
+++ app/src/main/java/com/example/scoccipe/projetphysique/Force_Centripete.java
@@ -1,5 +1,6 @@
 package com.example.scoccipe.projetphysique;
 
+import android.animation.AnimatorSet;
 import android.animation.ObjectAnimator;
 import android.content.Intent;
 import android.graphics.Path;
@@ -7,6 +8,8 @@ import android.graphics.RectF;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.AccelerateInterpolator;
+import android.view.animation.AnimationSet;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
@@ -14,13 +17,12 @@ import android.widget.Toast;
 public class Force_Centripete extends AppCompatActivity {
     public Intent intent;
     public boolean derapage;
+    public ImageView iv = findViewById(R.id.voit);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_force__centripete);
-
-        float x = 500, y = 500, r = 500, angle = 50;
 
         intent = getIntent();
         derapage = intent.getBooleanExtra("derap", ParametresCentripete.derap);
@@ -36,15 +38,17 @@ public class Force_Centripete extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 if(derapage){
-                    ImageView iv = findViewById(R.id.voit);
                     Path path = new Path();
-                    iv.animate().rotation(-360);
-                    iv.animate().setDuration(10000);
+                    path.addCircle(0,411, 450, Path.Direction.CCW);
+                    iv.animate().rotationBy(-360).setDuration(2000);
 
-                    path.arcTo(0f,30f,200f,1050f,-90f,-359,true);
+                    //ObjectAnimator animRot = ObjectAnimator.ofFloat(iv, "rotation", -360);
                     ObjectAnimator animator = ObjectAnimator.ofFloat(iv,View.X,View.Y,path);
-                    animator.setDuration(10000);
-                    animator.start();
+
+                    //AnimatorSet xy = new AnimatorSet();
+                    //xy.playTogether(animRot,animator);
+
+                    animator.setDuration(2000).start();
                 }
                 else{
 
