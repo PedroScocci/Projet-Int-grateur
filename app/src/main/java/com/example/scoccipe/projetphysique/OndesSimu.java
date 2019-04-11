9e4465df6cc8ca10ceb60a068a1ef01f61540108
Bob <ilibou55555@gmail.com>
2019-04-10 21:25:41 -0400

    Animation extrémité fermer(suite)
    J'ai presque fini l'animation de la corde pour tout mode stationnaire quand la corde est fermer. J'ai aussi ajouter des images pour la machine qui fait les ondes et l'extremité fermer.

--- app/src/main/java/com/example/scoccipe/projetphysique/OndesSimu.java
+++ app/src/main/java/com/example/scoccipe/projetphysique/OndesSimu.java
@@ -29,10 +29,9 @@ public class OndesSimu extends AppCompatActivity {
     private ImageView machine, extremite, cordes[];
     private  Handler customHandler = new Handler();
 
-    private double mode;
-    private boolean started = false;
+    private boolean started = false, nonMode = false;
     private long tempsDepart = 0, tempsMiliSecs = 0,tempsTemporaire = 0;
-    private int imageActuel[], sensImage[], reverseImage[], images[];
+    private int modeStatio, imageActuel[], imageMachine[], sensImage[], reverseImage[], images[], position;
 
 
     Runnable updateTimerThread = new Runnable() {
@@ -47,26 +46,29 @@ public class OndesSimu extends AppCompatActivity {
 
             txtTimer.setText(""+mins+":"+String.format("%2d",secs) + ":" + String.format("%3d",miliseconds));
 
-            //Toast.makeText(OndesSimu.this, String.valueOf(tempsMiliSecs), Toast.LENGTH_LONG).show();
-
             if(tempsMiliSecs >= tempsTemporaire + 1 && tempsMiliSecs <= tempsTemporaire + 60  ) { //Si le téléphone est performant
                 tempsTemporaire = tempsMiliSecs;
-                for(int i = 0; i < 3; i++) {
+
+                changerMachine();
+                for(int i = 0; i < modeStatio; i++) {
                     changerImage(i);
                 }
 
             } else if(tempsMiliSecs >= tempsTemporaire + 61 && tempsMiliSecs <= tempsTemporaire + 500 ) { //Si le téléphone est plus ou moins performant
                 tempsTemporaire = tempsMiliSecs;
-                for(int i = 0; i < 3; i++) {
+                changerMachine();
+                for(int i = 0; i < modeStatio; i++) {
                     changerImage(i);
                 }
 
             } else if(tempsMiliSecs >= tempsTemporaire + 501 && tempsMiliSecs <= tempsTemporaire + 1500 ) { //Si le téléphone n'est pas très performant
                 tempsTemporaire = tempsMiliSecs;
-                for(int i = 0; i < 3; i++) {
+                changerMachine();
+                for(int i = 0; i < modeStatio; i++) {
                     changerImage(i);
                 }
             }
+
 
             customHandler.postDelayed(this, 0);
         }
@@ -78,82 +80,71 @@ public class OndesSimu extends AppCompatActivity {
         setContentView(R.layout.activity_ondes_simu);
 
         intent = getIntent();
-        mode = Double.parseDouble(intent.getStringExtra(OndesParam.MODE_STATIONNAIRE));
-
-        Toast.makeText(OndesSimu.this, String.valueOf(mode), Toast.LENGTH_LONG).show();// à enlever après
-
-        imageActuel = new int[]{0,27,0};
-        sensImage = new int[]{0,0,0};
-        reverseImage = new int[]{0,1,0};
+        double mode = Double.parseDouble(intent.getStringExtra(OndesParam.MODE_STATIONNAIRE));
 
         machine = new ImageView(this);
-        machine.setImageResource(R.drawable.machine_corde_1);
+        machine.setImageResource(R.drawable.machine_corde_fermer);
         machine.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT, 1));
 
-        cordes = new ImageView[5];
-        for(int i = 0; i < 5; i++){
-            cordes[i] = new ImageView(this);
-            cordes[i].setImageResource(R.drawable.corde_onde_1);
-            cordes[i].setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT, 1));
-        }
-
         extremite = new ImageView(this);
-        extremite.setImageResource(R.drawable.machine_corde_1);
+        extremite.setImageResource(R.drawable.extremite_fermer);
         extremite.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT, 1));
 
-        cordes[0].setImageResource(R.drawable.corde_onde_1);
         txtTimer = (TextView) findViewById(R.id.timerValue);
         layout = (LinearLayout) findViewById(R.id.cordeLayout);
         layout.addView(machine);
-        layout.addView(cordes[0]);
-        layout.addView(cordes[1]);
-        layout.addView(cordes[2]);
+
+        if(mode == Math.round(mode) && mode > 20) {
+            modeStatio = (int)(mode)%2 + 20;
+            Toast.makeText(OndesSimu.this, String.valueOf(modeStatio), Toast.LENGTH_LONG).show();
+            initialiserVariable(modeStatio);
+        } else if(mode == Math.round(mode)){
+            modeStatio = (int) mode;
+            Toast.makeText(OndesSimu.this, "Nope", Toast.LENGTH_LONG).show();
+            initialiserVariable(modeStatio);
+        } else {
+            nonMode = true;
+        }
+
         layout.addView(extremite);
-
-
-        images = new int[]{R.drawable.corde_onde_1,R.drawable.corde_onde_2,R.drawable.corde_onde_3, R.drawable.corde_onde_4,  R.drawable.corde_onde_5,
-                R.drawable.corde_onde_6, R.drawable.corde_onde_7, R.drawable.corde_onde_8, R.drawable.corde_onde_9, R.drawable.corde_onde_10, R.drawable.corde_onde_11,
-                R.drawable.corde_onde_12, R.drawable.corde_onde_13, R.drawable.corde_onde_14, R.drawable.corde_onde_15, R.drawable.corde_onde_16,
-                R.drawable.corde_onde_17, R.drawable.corde_onde_18, R.drawable.corde_onde_19, R.drawable.corde_onde_20, R.drawable.corde_onde_21,
-                R.drawable.corde_onde_22, R.drawable.corde_onde_23, R.drawable.corde_onde_24, R.drawable.corde_onde_25, R.drawable.corde_onde_26,
-                R.drawable.corde_onde_27, R.drawable.corde_onde_r_2, R.drawable.corde_onde_r_3, R.drawable.corde_onde_r_4, R.drawable.corde_onde_r_5,
-                R.drawable.corde_onde_r_6, R.drawable.corde_onde_r_7, R.drawable.corde_onde_r_8, R.drawable.corde_onde_r_9, R.drawable.corde_onde_r_10,
-                R.drawable.corde_onde_r_11, R.drawable.corde_onde_r_12, R.drawable.corde_onde_r_13, R.drawable.corde_onde_r_14, R.drawable.corde_onde_r_15,
-                R.drawable.corde_onde_r_16, R.drawable.corde_onde_r_17, R.drawable.corde_onde_r_18, R.drawable.corde_onde_r_19, R.drawable.corde_onde_r_20,
-                R.drawable.corde_onde_r_21, R.drawable.corde_onde_r_22, R.drawable.corde_onde_r_23, R.drawable.corde_onde_r_24, R.drawable.corde_onde_r_25,
-                R.drawable.corde_onde_r_26}; // 52 cases
 
         start = (Button) findViewById(R.id.bOndesSimu);
         start.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(!started){
-                    tempsDepart = SystemClock.uptimeMillis();
+                if(!nonMode){
+                    if(!started){
+                        tempsDepart = SystemClock.uptimeMillis();
 
-                    customHandler.postDelayed(updateTimerThread, 0);
+                        customHandler.postDelayed(updateTimerThread, 0);
 
-                    start.setText("ARRÊTER");
-                    started = true;
-                }
-                else if(started){
-                    for(int y = 0; y < 3; y++){
-                        cordes[y].setImageResource(images[0]);
-                        if(y%2 == 1){
-                            reverseImage[y] = 1;
-                            imageActuel[y] = 27;
-                        }
-                        else if(y%2 == 0){
-                            reverseImage[y] = 0;
-                            imageActuel[y] = 0;
-                        }
-
-                        sensImage[y] = 0;
+                        start.setText("ARRÊTER");
+                        started = true;
                     }
-                    tempsTemporaire = 0;
-                    customHandler.removeCallbacks(updateTimerThread);
+                    else if(started){
+                        for(int y = 0; y < modeStatio; y++){
+                            cordes[y].setImageResource(images[0]);
 
-                    start.setText("COMMENCER");
-                    started = false;
+                            if(y%2 == 1){
+                                reverseImage[y] = 1;
+                                imageActuel[y] = 27;
+                            }
+                            else if(y%2 == 0){
+                                reverseImage[y] = 0;
+                                imageActuel[y] = 0;
+                            }
+
+                            sensImage[y] = 0;
+                        }
+                        tempsTemporaire = 0;
+                        machine.setImageResource(R.drawable.machine_corde_fermer);
+                        customHandler.removeCallbacks(updateTimerThread);
+
+                        start.setText("COMMENCER");
+                        started = false;
+                    }
+                } else {
+                    //à faire quand il ne s'agit pas d'un mode stationnaire
                 }
             }
         });
@@ -183,52 +174,101 @@ public class OndesSimu extends AppCompatActivity {
         }
     }
 
-    public void changerImage(int location) {
-            if(reverseImage[location] == 0){
-                if(sensImage[location] == 0) {
-                    if (imageActuel[location] < 26) {
-                        imageActuel[location]++;
-                        cordes[location].setImageResource(images[imageActuel[location]]);
-                    } else if (imageActuel[location] == 26) {
-                        sensImage[location]++;
-                        imageActuel[location]--;
-                        cordes[location].setImageResource(images[imageActuel[location]]);
-                    }
-                } else if(sensImage[location] == 1) {
-                    if (imageActuel[location] > 0) {
-                        imageActuel[location]--;
-                        cordes[location].setImageResource(images[imageActuel[location]]);
-                    } else if (imageActuel[location] == 0) {
-                        sensImage[location]--;
-                        reverseImage[location]++;
-                        imageActuel[location] = 27;
-                        cordes[location].setImageResource(images[imageActuel[location]]);
-                    }
-                }
+    public  void changerMachine(){
+        machine.setImageResource(imageMachine[position]);
+        position++;
+        if(position == 12){
+            position = 0;
+        }
+    }
 
-            } else if(reverseImage[location] == 1){
-                if(sensImage[location] == 0) {
-                    if (imageActuel[location] < 51) {
-                        imageActuel[location]++;
-                        cordes[location].setImageResource(images[imageActuel[location]]);
-                    } else if (imageActuel[location] == 51) {
-                        sensImage[location]++;
-                        imageActuel[location]--;
-                        cordes[location].setImageResource(images[imageActuel[location]]);
-                    }
-                } else if(sensImage[location] == 1) {
-                    if (imageActuel[location] > 27) {
-                        imageActuel[location]--;
-                        cordes[location].setImageResource(images[imageActuel[location]]);
-                    } else if (imageActuel[location] == 27) {
-                        sensImage[location]--;
-                        reverseImage[location]--;
-                        imageActuel[location] = 0;
-                        cordes[location].setImageResource(images[imageActuel[location]]);
-                    }
+    public void changerImage(int location) {
+        if(reverseImage[location] == 0){
+            if(sensImage[location] == 0) {
+                if (imageActuel[location] < 26) {
+                    imageActuel[location]++;
+                    cordes[location].setImageResource(images[imageActuel[location]]);
+                } else if (imageActuel[location] == 26) {
+                    sensImage[location]++;
+                    imageActuel[location]--;
+                    cordes[location].setImageResource(images[imageActuel[location]]);
                 }
-            } else {
-                cordes[location].setImageResource(R.drawable.message_erreur);
+            } else if(sensImage[location] == 1) {
+                if (imageActuel[location] > 0) {
+                    imageActuel[location]--;
+                    cordes[location].setImageResource(images[imageActuel[location]]);
+                } else if (imageActuel[location] == 0) {
+                    sensImage[location]--;
+                    reverseImage[location]++;
+                    imageActuel[location] = 27;
+                    cordes[location].setImageResource(images[imageActuel[location]]);
+                }
             }
+
+        } else if(reverseImage[location] == 1){
+            if(sensImage[location] == 0) {
+                if (imageActuel[location] < 51) {
+                    imageActuel[location]++;
+                    cordes[location].setImageResource(images[imageActuel[location]]);
+                } else if (imageActuel[location] == 51) {
+                    sensImage[location]++;
+                    imageActuel[location]--;
+                    cordes[location].setImageResource(images[imageActuel[location]]);
+                }
+            } else if(sensImage[location] == 1) {
+                if (imageActuel[location] > 27) {
+                    imageActuel[location]--;
+                    cordes[location].setImageResource(images[imageActuel[location]]);
+                } else if (imageActuel[location] == 27) {
+                    sensImage[location]--;
+                    reverseImage[location]--;
+                    imageActuel[location] = 0;
+                    cordes[location].setImageResource(images[imageActuel[location]]);
+                }
+            }
+        } else cordes[location].setImageResource(R.drawable.message_erreur);
+    }
+
+    private void initialiserVariable(int mode) {
+        imageActuel = new int[mode];
+        sensImage = new int[mode];
+        reverseImage = new int[mode];
+        cordes = new ImageView[mode];
+        position = 0;
+
+        for(int i = 0; i < mode; i++) {
+            sensImage[i] = 0;
+
+            if(i%2 == 0){
+                imageActuel[i] = 0;
+                reverseImage[i] = 0;
+            } else if(i%2 == 1){
+                imageActuel[i] = 27;
+                reverseImage[i] = 1;
+            }
+
+            cordes[i] = new ImageView(this);
+            cordes[i].setImageResource(R.drawable.corde_onde_1);
+            cordes[i].setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT, 1));
+
+            layout.addView(cordes[i]);
+        }
+
+        images = new int[]{R.drawable.corde_onde_1, R.drawable.corde_onde_2, R.drawable.corde_onde_3, R.drawable.corde_onde_4,  R.drawable.corde_onde_5,
+                R.drawable.corde_onde_6, R.drawable.corde_onde_7, R.drawable.corde_onde_8, R.drawable.corde_onde_9, R.drawable.corde_onde_10, R.drawable.corde_onde_11,
+                R.drawable.corde_onde_12, R.drawable.corde_onde_13, R.drawable.corde_onde_14, R.drawable.corde_onde_15, R.drawable.corde_onde_16,
+                R.drawable.corde_onde_17, R.drawable.corde_onde_18, R.drawable.corde_onde_19, R.drawable.corde_onde_20, R.drawable.corde_onde_21,
+                R.drawable.corde_onde_22, R.drawable.corde_onde_23, R.drawable.corde_onde_24, R.drawable.corde_onde_25, R.drawable.corde_onde_26,
+                R.drawable.corde_onde_27, R.drawable.corde_onde_r_2, R.drawable.corde_onde_r_3, R.drawable.corde_onde_r_4, R.drawable.corde_onde_r_5,
+                R.drawable.corde_onde_r_6, R.drawable.corde_onde_r_7, R.drawable.corde_onde_r_8, R.drawable.corde_onde_r_9, R.drawable.corde_onde_r_10,
+                R.drawable.corde_onde_r_11, R.drawable.corde_onde_r_12, R.drawable.corde_onde_r_13, R.drawable.corde_onde_r_14, R.drawable.corde_onde_r_15,
+                R.drawable.corde_onde_r_16, R.drawable.corde_onde_r_17, R.drawable.corde_onde_r_18, R.drawable.corde_onde_r_19, R.drawable.corde_onde_r_20,
+                R.drawable.corde_onde_r_21, R.drawable.corde_onde_r_22, R.drawable.corde_onde_r_23, R.drawable.corde_onde_r_24, R.drawable.corde_onde_r_25,
+                R.drawable.corde_onde_r_26}; // 52 cases
+
+        imageMachine = new int[]{R.drawable.machine_corde_ouvert, R.drawable.machine_corde_ouvert_2, R.drawable.machine_corde_ouvert_3,
+                R.drawable.machine_corde_ouvert_4, R.drawable.machine_corde_ouvert_3, R.drawable.machine_corde_ouvert_2,
+                R.drawable.machine_corde_ouvert, R.drawable.machine_corde_ouvert_r_2, R.drawable.machine_corde_ouvert_r_3,
+                R.drawable.machine_corde_ouvert_r_4, R.drawable.machine_corde_ouvert_r_3, R.drawable.machine_corde_ouvert_r_2}; // 12 cases
     }
 }
